e8a04f51edf89732a7ca4c7dcaaf63ee51b3d185
Marcus Maione <marcusmaione@hotmail.com>
2019-04-18 00:05:09 -0300

    all profile registration process

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_13_040610) do
+ActiveRecord::Schema.define(version: 2019_04_18_021114) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,7 +29,7 @@ ActiveRecord::Schema.define(version: 2019_04_13_040610) do
   end
 
   create_table "courses", force: :cascade do |t|
-    t.string "type"
+    t.string "category"
     t.string "school_name"
     t.text "description"
     t.string "complete"
