ae390311ca46e96d83eb93ecb47aa8428ed0fa75
Marcus Maione <marcusmaione@hotmail.com>
2019-05-01 18:22:40 -0300

    users controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,45 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  skip_before_action :authenticate_user!, only: [:show]
+
+  def show
+  end
+
+  def new
+    @user = current_user
+  end
+
+  def create
+    @user = current_user
+    if @user.save(detail_params)
+      redirect_to profile_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(detail_params)
+      if current_user.degrees.empty?
+        redirect_to new_degree_path
+      else
+        redirect_to profile_path
+      end
+    else
+      render :update
+    end
+  end
+
+  private
+
+  def set_user
+    @user = current_user
+  end
+
+  def detail_params
+    params.require(:user).permit(:first_name, :last_name, :phone, :avatar)
+  end
+end
