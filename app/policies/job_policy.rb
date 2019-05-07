ca70b1f41baa444453adf85df049a6e2669fc22d
Marcus Maione <marcusmaione@hotmail.com>
2019-05-07 00:34:40 -0300

    jobs changes

---
+++ app/policies/job_policy.rb
@@ -0,0 +1,31 @@
+class JobPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.where(user: user)
+    end
+  end
+
+  def index?
+    return true
+  end
+
+  def create?
+    return true
+  end
+
+  def edit?
+    record.company.user == user
+  end
+
+  def show?
+    return true
+  end
+
+  def update?
+    record.company.user == user
+  end
+
+  def destroy?
+    record.company.user == user
+  end
+end
